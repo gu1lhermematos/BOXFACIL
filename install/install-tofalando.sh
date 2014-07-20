8cca24903cf308847d409a5d8a23b74895b8bf75
gu1lhermematos <guilhermematos@Mac-mini-de-Guilherme.local>
2014-07-20 18:19:46 -0300

    teste

--- install/install-tofalando.sh
+++ install/install-tofalando.sh
@@ -13,7 +13,7 @@ rm -rf ipbx
 
 cd /usr/src/
 
-git clone https://github.com/gu1lhermematos/VOXIPBX.git
+git clone https://github.com/gu1lhermematos/BOXFACIL.git
 cd VOXIPBX
 git branch $BRANCH
 git pull origin $BRANCH
